2e9f8357afe2e5451217531cd3af2a813bc2aa7e
ishanrana363 <ishanrana363@gmail.com>
2024-12-28 22:59:48 +0600

    data type completed paython

--- module-2/data.py
+++ module-2/data.py
@@ -1,60 +1,106 @@
 # python data type
 
-age = 50   # integer
+# age = 50   # integer
 
-print(age)
+# print(age)
 
-number = 85.5 # flot data
+# number = 85.5 # flot data
 
-print(number)
+# print(number)
 
-complexNumber = 20+ 20J
+# complexNumber = 20+ 20J
 
-print(complexNumber)
+# print(complexNumber)
 
 
-name = "MD. Ishan Rana"  #String
+# name = "MD. Ishan Rana"  #String
 
-print(name)
+# print(name)
 
 #sequence data type
 
 # list mutable
 
-language = ["python","javaScript","C","C++","Ruby","Go"]   # list
+# language = ["python","javaScript","C","C++","Ruby","Go"]   # list
 
-print(language[1])
+"""print(language[1])
 print(language[4])
-print(language)
+print(language)"""
+
+# print(len(language))
 
 # tuple immutable
 
 
-city = ("Dinajpur","Rangpur","Dhaka","Gagipur")
-city = ("Dinajpur","Rangpur","Dhaka","Gagipur",1,3,4,5,6)
-print(city[1])
+# city = ("Dinajpur","Rangpur","Dhaka","Gagipur")
+# city = ("Dinajpur","Rangpur","Dhaka","Gagipur",1,3,4,5,6)
+# print(city[1])
 
-print(city)
+# print(city)
+#
+# # range data type
+#
+# numbers = range(0,19,3)
+#
+# print(*numbers)
 
-# range data type
+# print(len(city))
 
-numbers = range(0,19,3)
-
-print(*numbers)
-
-
-
-
-
-
-
-
-
+# print(city[-1])
+# print(city[-2])
+# print(city[2:5])
+# print(city)
 
 
+# isBangladeshi = True
+#
+# print(isBangladeshi)
+#
+# isIndian = False
+#
+# print(isIndian)
+
+
+# set dictionary
+
+person = {
+    "first_name" : "Ishan",
+    "last_name" : "Rana",
+    "isBangladeshi" : True,
+    "height" : 5.5,
+    "height":5.11,
+    "favourite_language" : ["JavaScript","Python"],
+    "hobby" : ("Codding","Programming","Football","Cricket")
+}
+
+# print(person)
+
+# print(person["height"])
+
+# print(len(person))
+# print(person)
+
+
+# thisdict = dict(name = "John", age = 36, country = "Norway");
+#
+# print(thisdict)
+
+# fourth_dict = dict(name="ishan",roll=500510,reg=1502084202,age=21)
+# print(fourth_dict["age"])
+
+
+# set data type ---> mutable
+
+unique_number = {1,1,3,3,4,5,6,7}
+print(unique_number)
 
 
 
+
+
+
+
+# frozenset  immutable
 
 
 
