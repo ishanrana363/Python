ec0246efee9d23fb1c8e3117e8d26f397580c0b8
ishanrana363 <ishanrana363@gmail.com>
2025-01-09 22:47:00 +0600

    type casting completed

---
+++ data-type/main.py
@@ -0,0 +1,169 @@
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
+from pickletools import bytes1
+
+# Python Numbers
+
+# There are three numeric types in Python:
+# 1 int
+# 2 flot
+# 3 complex
+
+
+# 1 int
+# Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
+# a = 10
+# print(a)
+
+# 2 flot
+# Float, or "floating point number" is a number, positive or negative, containing one or more decimals.
+
+# pi = 3.1416
+
+# print(pi)
+
+# 3 complex
+
+# z = 5j+10
+#
+# print(type(z))
+
+
+# y = 10E1
+#
+# print(type(y))
+
+# b = 10
+# print(type(b))
+
+# import random
+#
+# randomNumber = random.randrange(100000,999999)
+#
+# print(randomNumber)
+
+# int() - constructs an integer number from an integer literal, a float literal (by removing all decimals), or a string literal (providing the string represents a whole number)
+
+# x = 4
+# y = 4.5
+# z = "10"
+#
+# a = int(x)
+# b = int(y)
+# c = int(z)
+#
+# print(type(a),type(b),type(c))
+
+
+
+# float() - constructs a float number from an integer literal, a float literal or a string literal (providing the string represents a float or an integer)
+
+
+
+# x = 4
+# y = 4.5
+# z = "10"
+#
+# a = float(x)
+# b = float(y)
+# c = float(z)
+#
+#
+# print(type(a),type(b),type(c))
+
+
+
+x = 4
+y = 4.5
+z = "10"
+
+a = str(x)
+b = str(y)
+c = str(z)
+print(a,b,c)
+
+
+print(type(a),type(b),type(c))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
